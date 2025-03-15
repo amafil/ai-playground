d78e3b653ae762828158155b7f5db5dddfdfe180
PLUSERVICE\f.amaduzzi <f.amaduzzi@pluservice.net>
2025-03-15 15:36:23 +0100

    Add knowledge variable for helpdesk task questions and answers

---
+++ knowledge.py
@@ -0,0 +1,14 @@
+# The `knowledge` variable contains all the knowledge about the helpdesk task.
+# It is structured as a list of tuples, where each tuple represents a question and its corresponding answer.
+# This data is used by FAISS (Facebook AI Similarity Search) for efficient similarity-based retrieval.
+
+knowledge = [
+    (
+        "question1",
+        "answer1",
+    ),
+    (
+        "question2",
+        "answer2",
+    ),
+]
